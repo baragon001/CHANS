1fae8166feb10970818bc95d7671edc19e7d6720
zk43 <zk43@cornell.edu>
2013-05-09 20:02:00 -0400

    include folder

---
+++ inc/contactform.php
@@ -0,0 +1,18 @@
+<h1>Get In Touch</h1>
+	<div id="message">
+	<form method="post" action="contact.php" id="contactform">
+	<table border="0">
+		<tr>
+			<td><input type="text" name="name" id="name" placeholder="Your Name"/></td>
+		</tr>
+		<tr>
+			<td><input type="text" name="email" id="email" placeholder="Your Email"/></td>
+		</tr>
+		<tr>
+			<td><textarea id="comments" name="comments" placeholder="Leave me a message"></textarea></td>
+		</tr>
+		<tr>
+			<td><button type="submit" id="submit">Send Message</button><td>
+		</tr>
+	</table>
+	</div>
---
+++ inc/loginform.php
@@ -0,0 +1,16 @@
+<h1>Login!</h1>
+<form id="lform" action="index.php" method="post">
+<table border="0">
+	<tr>
+		<td>Username:</td>
+		<td><input type="text" name="username" size="25" /> <br /></td>
+	</tr>
+	<tr>
+		<td>Password:</td>
+		<td><input type="password" name="password" size="25"/> <br /></td>
+	</tr>
+	<tr>
+		<td><input type="submit" value="Submit" /></td>
+	</tr>
+</table>	
+</form>
